285407666d55a54a858af283c5f880b861fc73bb
Basavilbaso <fbasavilbaso@udesa.edu.ar>
2023-11-14 14:03:42 -0300

    .

--- js/generos.js
+++ js/generos.js
@@ -1,7 +1,20 @@
 let TMDB_API_BASE_URL = 'https://api.themoviedb.org/3';
 
+let API_KEY = 'tu_clave_de_api';
+
 let MOVIE_GENRES_ENDPOINT = '/genre/movie/list';
 
-let TV_GENRES_ENDPOINT = '/genre/tv/list';
+let SERIES_GENRES_ENDPOINT = '/genre/series/list';
+
+async function obtenerGenerosPeliculas() {
+    try {
+        let response = await fetch(`${'https://api.themoviedb.org/3'}${'/genre/movie/list'}?api_key=${API_KEY}`); 
+        let data = await response.json();
+        return data.genres;
+    } catch (error) {
+        console.error('Error al obtener géneros de películas:', error);
+        return [];
+    }
+}
 
 
