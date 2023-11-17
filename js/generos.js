0f1cfdc6a418daf0dd5037c842e2104baa02d54d
bautista-lucini <143819604+bautista-lucini@users.noreply.github.com>
2023-11-17 12:48:06 -0300

    cambios al css

--- js/generos.js
+++ js/generos.js
@@ -1,5 +1,5 @@
-const apiKey = '15879dad47bfb7f22061a18ffdf1b790';
-const baseURL = 'https://api.themoviedb.org/3';
+let apiKey = '15879dad47bfb7f22061a18ffdf1b790';
+let baseURL = 'https://api.themoviedb.org/3';
 
 function fetchGenres(endpoint, targetElementId) {
   fetch(`${baseURL}${endpoint}?api_key=${apiKey}`)
@@ -18,12 +18,12 @@ function fetchGenres(endpoint, targetElementId) {
 }
 
 function displayGenres(genres, targetElementId) {
-  const targetElement = document.getElementById(targetElementId);
+  let targetElement = document.getElementById(targetElementId);
   targetElement.innerHTML = '';
 
   genres.forEach(genre => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
+    let listItem = document.createElement('li');
+    let link = document.createElement('a');
     link.href = '#';
     link.textContent = genre.name;
     link.onclick = function() {
@@ -52,7 +52,7 @@ function fetchGenreDetails(genreId, genreName) {
 }
 
 function displayGenreDetails(genreDetails, genreName) {
-  const genreDetailsElement = document.getElementById('genreDetails');
+  let genreDetailsElement = document.getElementById('genreDetails');
   genreDetailsElement.innerHTML = '';
 
   if (genreDetails.length === 0) {
@@ -61,21 +61,19 @@ function displayGenreDetails(genreDetails, genreName) {
     genreDetailsElement.appendChild(errorParagraph);
   } else {
     genreDetails.forEach(detail => {
-      const movieElement = document.createElement('div');
-      const movieTitle = document.createElement('p');
+      let movieElement = document.createElement('div');
+      let movieTitle = document.createElement('p');
       movieTitle.textContent = detail.title;
       movieElement.appendChild(movieTitle);
 
       if (detail.poster_path) {
-        const moviePoster = document.createElement('img');
+        let moviePoster = document.createElement('img');
         moviePoster.src = `https://image.tmdb.org/t/p/w200${detail.poster_path}`;
         movieElement.appendChild(moviePoster);
       }
 
       movieElement.onclick = function() {
-        // Handle click on movie/series for more details
         console.log(`Clicked on ${detail.title}`);
-        // Redirect to movie/series details page or perform any other action
       };
 
       genreDetailsElement.appendChild(movieElement);
@@ -83,7 +81,6 @@ function displayGenreDetails(genreDetails, genreName) {
   }
 }
 
-// Fetch movie and TV series genres when the page loads
 window.onload = function() {
   fetchGenres('/genre/movie/list', 'movieGenres');
   fetchGenres('/genre/tv/list', 'tvGenres');
