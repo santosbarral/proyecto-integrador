aeb497715ae16e79370a1e64accbb8680fe75e6a
santosbarral <santosbarral@github.com>
2023-11-17 16:50:41 -0300

    peliculas generos

--- js/generos.js
+++ js/generos.js
@@ -24,61 +24,11 @@ function displayGenres(genres, targetElementId) {
   genres.forEach(genre => {
     let listItem = document.createElement('li');
     let link = document.createElement('a');
-    link.href = '#';
+    link.href = `ver-generos.html?genreId=${genre.id}&genreName=${encodeURIComponent(genre.name)}`;
     link.textContent = genre.name;
-    link.onclick = function() {
-      fetchGenreDetails(genre.id, genre.name);
-    };
     listItem.appendChild(link);
     targetElement.appendChild(listItem);
   });
-}
-
-function fetchGenreDetails(genreId, genreName) {
-  fetch(`${baseURL}/discover/movie?api_key=${apiKey}&with_genres=${genreId}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Network response was not ok for ${genreName}.`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      displayGenreDetails(data.results, genreName);
-    })
-    .catch(error => {
-      console.error(`Error fetching genre details for ${genreName}:`, error);
-      displayGenreDetailsError();
-    });
-}
-
-function displayGenreDetails(genreDetails, genreName) {
-  let genreDetailsElement = document.getElementById('genreDetails');
-  genreDetailsElement.innerHTML = '';
-
-  if (genreDetails.length === 0) {
-    const errorParagraph = document.createElement('p');
-    errorParagraph.textContent = `No movies found for ${genreName}.`;
-    genreDetailsElement.appendChild(errorParagraph);
-  } else {
-    genreDetails.forEach(detail => {
-      let movieElement = document.createElement('div');
-      let movieTitle = document.createElement('p');
-      movieTitle.textContent = detail.title;
-      movieElement.appendChild(movieTitle);
-
-      if (detail.poster_path) {
-        let moviePoster = document.createElement('img');
-        moviePoster.src = `https://image.tmdb.org/t/p/w200${detail.poster_path}`;
-        movieElement.appendChild(moviePoster);
-      }
-
-      movieElement.onclick = function() {
-        console.log(`Clicked on ${detail.title}`);
-      };
-
-      genreDetailsElement.appendChild(movieElement);
-    });
-  }
 }
 
 window.onload = function() {
---
+++ js/ver-generos.js
@@ -0,0 +1,61 @@
+let apiKey = '15879dad47bfb7f22061a18ffdf1b790';
+let baseURL = 'https://api.themoviedb.org/3';
+
+function fetchGenreDetails(genreId, genreName) {
+  fetch(`${baseURL}/discover/movie?api_key=${apiKey}&with_genres=${genreId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok for ${genreName}.`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      displayGenreDetails(data.results, genreName);
+    })
+    .catch(error => {
+      console.error(`Error fetching genre details for ${genreName}:`, error);
+      displayGenreDetailsError();
+    });
+}
+
+function displayGenreDetails(genreDetails, genreName) {
+  let genreDetailsElement = document.getElementById('genreDetails');
+  genreDetailsElement.innerHTML = '';
+
+  if (genreDetails.length === 0) {
+    const errorParagraph = document.createElement('p');
+    errorParagraph.textContent = `No movies found for ${genreName}.`;
+    genreDetailsElement.appendChild(errorParagraph);
+  } else {
+    genreDetails.forEach(detail => {
+      let movieElement = document.createElement('div');
+      let movieTitle = document.createElement('p');
+      movieTitle.textContent = detail.title;
+      movieElement.appendChild(movieTitle);
+
+      if (detail.poster_path) {
+        let moviePoster = document.createElement('img');
+        moviePoster.src = `https://image.tmdb.org/t/p/w200${detail.poster_path}`;
+        movieElement.appendChild(moviePoster);
+      }
+
+      movieElement.onclick = function() {
+        console.log(`Clicked on ${detail.title}`);
+      };
+
+      genreDetailsElement.appendChild(movieElement);
+    });
+  }
+}
+
+window.onload = function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const genreId = urlParams.get('genreId');
+  const genreName = urlParams.get('genreName');
+
+  if (genreId && genreName) {
+    fetchGenreDetails(genreId, decodeURIComponent(genreName));
+  } else {
+    console.error('Genre ID and Name not provided in the URL.');
+  }
+};
