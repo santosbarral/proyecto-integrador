130b1395aee933a07951b9f79e665b2d7ceb5244
bautista-lucini <143819604+bautista-lucini@users.noreply.github.com>
2023-11-17 03:36:55 -0300

    bucador

--- js/resultado-busqueda.js
+++ js/resultado-busqueda.js
@@ -1,11 +1,11 @@
-const API_KEY = '15879dad47bfb7f22061a18ffdf1b790'; // Reemplaza 'tu_api_key' con tu API key de TMDb
-      const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+let API_KEY = '15879dad47bfb7f22061a18ffdf1b790'; 
+      let BASE_URL = 'https://api.themoviedb.org/3/search/movie';
   
-      const searchBox = document.getElementById('search-box');
-      const resultsContainer = document.getElementById('results');
+      let searchBox = document.getElementById('search-box');
+      let resultsContainer = document.getElementById('results');
   
       searchBox.addEventListener('keyup', function(event) {
-        const searchTerm = event.target.value;
+        let searchTerm = event.target.value;
   
         if (searchTerm.length > 2) {
           searchMovies(searchTerm);
@@ -15,11 +15,11 @@ const API_KEY = '15879dad47bfb7f22061a18ffdf1b790'; // Reemplaza 'tu_api_key' co
       });
   
       async function searchMovies(query) {
-        const url = `${BASE_URL}?api_key=${API_KEY}&query=${query}`;
+        let url = `${BASE_URL}?api_key=${API_KEY}&query=${query}`;
         
         try {
-          const response = await fetch(url);
-          const data = await response.json();
+          let response = await fetch(url);
+          let data = await response.json();
           displayResults(data.results);
         } catch (error) {
           console.error('Error al obtener datos:', error);
@@ -35,12 +35,12 @@ const API_KEY = '15879dad47bfb7f22061a18ffdf1b790'; // Reemplaza 'tu_api_key' co
         }
   
         movies.forEach(movie => {
-          const movieTitle = movie.title;
-          const movieReleaseDate = movie.release_date || 'Fecha de lanzamiento no disponible';
-          const movieOverview = movie.overview || 'Descripción no disponible';
-          const moviePoster = movie.poster_path ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : 'https://via.placeholder.com/150';
+          let movieTitle = movie.title;
+          let movieReleaseDate = movie.release_date || 'Fecha de lanzamiento no disponible';
+          let movieOverview = movie.overview || 'Descripción no disponible';
+          let moviePoster = movie.poster_path ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : 'https://via.placeholder.com/150';
   
-          const movieElement = `
+          let movieElement = `
             <div>
               <img src="${moviePoster}" alt="${movieTitle}">
               <h3>${movieTitle}</h3>
